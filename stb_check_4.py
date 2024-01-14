0e2a8c539cf8edf19e5adae51c5bd78508ba6075
frkg0325 <154652705+frkg0325@users.noreply.github.com>
2024-01-14 12:33:20 +0900

    Add files via upload

--- stb_check_4.py
+++ stb_check_4.py
@@ -109,10 +109,12 @@ if selected_pre == "東京都":
 else:
     # 東京以外
     #緯度、経度
+    st.write(selected_pre)
     ido = df_pre[df_pre["都道府県"] == selected_pre]["緯度"].values[0]
     keido = df_pre[df_pre["都道府県"] == selected_pre]["経度"].values[0]
     # 拡大率
     df_zoom = df_pre[df_pre["都道府県"].str.contains(selected_pre)]
+    st.write(df_zoom)
     zoom_set = df_zoom["拡大率"].values.tolist()[0]
 
 
